4d9c25b9021e0c16b85be98a23f67c6008cdc903
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-07 13:54:29 -0300

    Variáveis

---
+++ Modulo-01/Algoritimos/src/Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis {
+
+    // Variável é a representação simbólica dos elementos de um
+    // certo conjunto.
+
+    private static int produto = 10;
+    private static int quantidade = 3;
+
+    private static int total = produto * quantidade;
+
+    public static void main(String[] arg){
+        total = (produto * quantidade) * 5;
+    }
+}
