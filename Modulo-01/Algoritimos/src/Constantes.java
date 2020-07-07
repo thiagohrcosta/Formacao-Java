9884c76ab26f6a997ef47ba6055447f849153360
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-07 12:45:27 -0300

    Constantes

---
+++ Modulo-01/Algoritimos/src/Constantes.java
@@ -0,0 +1,14 @@
+public class Constantes {
+
+
+        // Constante é um determinado valor fixo que não se
+        // modifica ao longo do tempo, durante a execução
+        // de um determinad programa.
+
+        private static final String TIPO_JURIDICA = "Juridica";
+        private static final boolean ATIVO = true;
+        private static final int MEDIA = 80;
+
+
+
+}
