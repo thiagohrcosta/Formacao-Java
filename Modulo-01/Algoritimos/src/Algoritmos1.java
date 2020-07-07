1fe4d472dec27d161db4049bc28440e3d4e1bdac
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-06 22:41:25 -0300

    Iniciando estudos de algoritmo

---
+++ Modulo-01/Algoritimos/src/Algoritmos1.java
@@ -0,0 +1,11 @@
+public class Algoritmos1 {
+
+    public static void main(String[] args){
+
+        int retangulo_A = 5;
+        int retangulo_B = 5;
+
+        System.out.println("Resultado = " + (retangulo_A + retangulo_B));
+        
+    }
+}
