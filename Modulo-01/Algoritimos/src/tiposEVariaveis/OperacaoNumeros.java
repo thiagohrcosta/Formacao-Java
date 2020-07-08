7b3a46bc2f5ba87823c987f679a1fd5181dd6e7b
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-08 20:54:24 -0300

    Tipos e variáveis

---
+++ Modulo-01/Algoritimos/src/tiposEVariaveis/OperacaoNumeros.java
@@ -0,0 +1,25 @@
+package tiposEVariaveis;
+
+public class OperacaoNumeros {
+
+    public static void main(String[] args){
+
+        int nota1 = 90;
+        int nota2 = 80;
+        int nota3 = 70;
+        int nota4 = 50;
+
+        System.out.println("Média das notas = " + ((nota1 + nota2 + nota3 + nota4) / 4));
+
+        double nota5 = 90;
+        double nota6 = 80;
+        double nota7 = 70;
+        double nota8 = 50;
+
+        double media = ((nota5 + nota6 + nota7 + nota8) / 4);
+        System.out.println("Media das notas = " + media);
+
+
+
+    }
+}
---
+++ Modulo-01/Algoritimos/src/tiposEVariaveis/tiposEVariaveis.java
@@ -0,0 +1,11 @@
+package tiposEVariaveis;
+
+public class tiposEVariaveis {
+
+    // byte tipoByte;
+    // short tipoShort;
+    // int tipoInt;
+    // boolean tipoBoolean;
+    // String tipoTexto;
+    // BigDecimal tipoBigDecimal;
+}
