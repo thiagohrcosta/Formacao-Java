839e446f42d97c6920b94ba96c2ba565330c9eee
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-09 12:34:57 -0300

    Operadores relacionais

---
+++ Modulo-01/Algoritimos/src/OperacoesLogicas/OperadoresRelacionais.java
@@ -0,0 +1,17 @@
+package OperacoesLogicas;
+
+public class OperadoresRelacionais {
+
+    public static void main(String[] args){
+        int nota1 = 90;
+        int nota2 = 80;
+        int nota3 = 20;
+
+        if(nota1 == nota2){
+            System.out.println("as notas são iguais");
+        }
+        else{
+            System.out.println("as notas são diferentes");
+        }
+    }
+}
