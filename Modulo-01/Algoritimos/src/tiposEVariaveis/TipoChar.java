e4c3514074a17e1317e38cd0152f3995abf0c396
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-08 21:03:58 -0300

    Tipo Char

---
+++ Modulo-01/Algoritimos/src/tiposEVariaveis/TipoChar.java
@@ -0,0 +1,25 @@
+package tiposEVariaveis;
+
+public class TipoChar {
+
+    /* CHAR representa uma letra */
+
+    public static void main(String[] args){
+        //char pessoaFisica = 'F';
+        //char pessoaJuridica = 'J';
+
+        char pessoaFeminino = 'F';
+
+        if(pessoaFeminino == 'F'){
+            System.out.println("Feminino");
+        }
+        else{
+            System.out.println("Masculino");
+        }
+
+
+
+
+
+    }
+}
