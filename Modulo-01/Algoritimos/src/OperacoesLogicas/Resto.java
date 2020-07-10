0606184dc1796f6556611c40b019fd863aea3635
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-10 16:08:33 -0300

    Operações com o resto da divisão

---
+++ Modulo-01/Algoritimos/src/OperacoesLogicas/Resto.java
@@ -0,0 +1,15 @@
+package OperacoesLogicas;
+
+public class Resto {
+
+    public static void main(String[] args){
+
+        int numero1 = 10;
+        int numero2= 2;
+
+        int resto = numero1 % numero2;
+
+        System.out.println(resto);
+
+    }
+}
