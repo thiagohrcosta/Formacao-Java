203539054813ede1f4e1f2d330073d20a60766a1
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-11 16:05:55 -0300

    Criando objetos na POO

---
+++ Modulo-02-OrientacaoObjetos/src/EstudoInicial/Aluno.java
@@ -0,0 +1,16 @@
+package EstudoInicial;
+
+import java.util.Date;
+
+public class Aluno {
+
+    String nome;
+    int idade;
+
+    // Neste momento do curso utilizou-se a String para dataNascimento;
+    String dataNascimento;
+    String registroGeral;
+    String numeroCpf;
+    String nomeMae;
+
+}
