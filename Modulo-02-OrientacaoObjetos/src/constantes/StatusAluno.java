04d6c91e984c68cd0a86375924a26b2fc9cfc764
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-20 13:10:43 -0300

    Criando classes e atributos estáticos

---
+++ Modulo-02-OrientacaoObjetos/src/constantes/StatusAluno.java
@@ -0,0 +1,10 @@
+package constantes;
+
+public class StatusAluno {
+
+    public static String APROVADO = "Aprovado";
+    public static String RECUPERACAO = "Recuperacao";
+    public static String REPROVADO = "Reprovado";
+    
+
+}
